c5ab7e8c44a0959fb94578de7c71c289e5383ba9
Declan Sheridan <declan.sheridan@gmail.com>
2022-10-30 12:56:21 +0000

    updates

--- _next/static/0LnDCTcC69cZ6YQiefea0/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Ftags","\u002F","\u002Ffixtures","\u002Fnews","\u002Fnews\u002F[...slug]","\u002Fnews\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
