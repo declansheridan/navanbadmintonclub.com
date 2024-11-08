5fa99549a71c288f4df55e01a674ff18ebcac675
Declan Sheridan <declan.sheridan@gmail.com>
2024-11-08 18:34:26 +0000

    Fri Nov  8 18:34:26 UTC 2024 latest release

--- _next/static/U1X21xKUdNnIs_f7QQiDj/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ffixtures","\u002Ftags","\u002Fnews","\u002Fabout","\u002F","\u002Fnews\u002F[...slug]","\u002Fnews\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
